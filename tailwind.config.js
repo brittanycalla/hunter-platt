9b7468b7da1e64aa5f0442c1e052501ad847c5bc
Brittany Callahan <brittanycallahanx@gmail.com>
2023-01-22 15:23:17 -0500

    add initial layout

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'teal-1000': 'rgb(0, 63, 45)',
+      },
+      height: {
+        '50vh': '50vh',
+      }
+    },
+  },
+  plugins: [],
+}
